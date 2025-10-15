f07a668a213c09831110316ad798a8aecf51de8d
David <52219585+DavidPolos@users.noreply.github.com>
2025-10-15 13:04:14 +0200

    Gram_map basic structure

--- src/main.cpp
+++ src/main.cpp
@@ -3,9 +3,10 @@
 using namespace std;
 #include <sstream>
 
-struct gram_entry {
+struct Gram_entry {
     int freq;
     map<char, int> trans_freq;
+    char next;
 };
 
 class Gram_map {
@@ -15,27 +16,56 @@ class Gram_map {
     void make_map(string s) {
         for (int i = 0; i < s.length() - k; ++i) {
             string gram = s.substr(i, k);
-            gram_freq[gram][s[i + k]]++;
+            gram_freq[gram].freq++;
+            gram_freq[gram].trans_freq[s[i + k]]++;
         }
+        condense();
+    }
+
+    string get_initial() {
+        string out;
+        int max = 0;
+        for (auto& element : gram_freq) {
+            if (element.second.freq > max) {
+                max = element.second.freq;
+                out = element.first;
+            }
+        }
+        return out;
+    }
+
+    char next(string s) {
+        return gram_freq[s].next;
     }
 
     void str() {
         for (auto& p : gram_freq) {
-            cout << p.first << " :{ " << endl;
-            for (auto& q : p.second) {
-                cout << q.first << ": " << q.second << ", " << endl;
-            }
-            cout << "}" << endl;
+            cout << p.first << "," << p.second.freq << " : " << p.second.next << endl;
         }
     }
 
    private:
     int k;
-    map<string, map<char, int>> gram_freq;
+    map<string, Gram_entry> gram_freq;
+
+    void condense() {
+        for (auto& element : gram_freq) {
+            int max = 0;
+            for (auto& trans_pair : element.second.trans_freq) {
+                if (trans_pair.second > max) {
+                    max = trans_pair.second;
+                    element.second.next = trans_pair.first;
+                }
+            }
+            element.second.trans_freq.clear();
+        }
+    }
 };
 
 int main() {
     Gram_map m{2};
-    m.make_map("ababac");
+    m.make_map("ababacbacbaabbacba");
     m.str();
+    cout << m.get_initial() << endl;
+    cout << m.next("ac");
 }
